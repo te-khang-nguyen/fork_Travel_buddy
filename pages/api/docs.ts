426cd2efffbdddcba8d2e81babd025b3731778ad
TE-Khang Nguyen <khang.nguyen@talentedge.ai>
2025-02-13 10:33:42 +0000

    Sync with upstream, excluding workflows

--- pages/api/docs.ts
+++ pages/api/docs.ts
@@ -1,7 +1,47 @@
 // pages/api/docs.js
 
-import { swaggerOptions } from '@/app/utils/swagger';
+
 import swaggerJsdoc from 'swagger-jsdoc';
+
+import * as path from 'path';
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Travel Buddy API Documentation',
+    version: '1.0.0',
+    description: 'API documentation for the Travel Buddy application',
+  },
+  servers: [
+    {
+      url: 'https://fork-travel-buddy.vercel.app/', // Add your production URL
+      description: 'Production server'
+    }
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'Enter your bearer token in the format "Bearer <token>"'
+      }
+    }
+  },
+  security: [
+    {
+      bearerAuth: []
+    }
+  ]
+};
+
+export const swaggerOptions = {
+  swaggerDefinition,
+  apis: [
+    './pages/api/**/*.js',
+    './pages/api/**/*.ts',
+  ],
+};
 
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
