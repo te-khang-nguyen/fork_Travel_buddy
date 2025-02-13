509324a84509f508378c261ed8a51b583b55bffb
TE-Khang Nguyen <khang.nguyen@talentedge.ai>
2025-02-13 11:05:46 +0000

    Sync with upstream, excluding workflows

--- pages/api/docs.ts
+++ pages/api/docs.ts
@@ -1,58 +1,35 @@
-// pages/api/docs.js
+import { createSwaggerSpec } from "next-swagger-doc";
 
-
-import swaggerJsdoc from 'swagger-jsdoc';
-
-import * as path from 'path';
-
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Travel Buddy API Documentation',
-    version: '1.0.0',
-    description: 'API documentation for the Travel Buddy application',
-  },
-  servers: [
-    {
-      url: 'https://fork-travel-buddy.vercel.app/', // Add your production URL
-      description: 'Production server'
-    }
-  ],
-  components: {
-    securitySchemes: {
-      bearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        description: 'Enter your bearer token in the format "Bearer <token>"'
-      }
-    }
-  },
-  security: [
-    {
-      bearerAuth: []
-    }
-  ]
+export const getApiDocs = async () => {
+  const spec = createSwaggerSpec({
+    apiFolder: "pages/api", // define api folder under app folder
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Travel Buddy API",
+        version: "1.0.0",
+        description: "API documentation for the Travel Buddy project",
+      },
+      components: {
+        securitySchemes: {
+          BearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      },
+      security: [
+        {
+          BearerAuth: [],
+        },
+      ],
+    },
+  });
+  return spec;
 };
 
-
-export const swaggerOptions = {
-  swaggerDefinition,
-  apis: [
-    path.join(process.cwd(), 'pages/api/**/*.js'),
-    path.join(process.cwd(), 'pages/api/**/*.ts'),
-  ],
+export default async (req, res) => {
+  const spec = await getApiDocs();
+  res.json(spec);
 };
-
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(swaggerSpec);
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
