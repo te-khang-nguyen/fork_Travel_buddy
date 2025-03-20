c5d4bd47a0ed549106df7cf5fc9cc6a591a06115
TE-Khang Nguyen <khang.nguyen@talentedge.ai>
2025-03-20 09:06:42 +0000

    Sync with upstream, excluding workflows

--- middleware.ts
+++ middleware.ts
@@ -23,13 +23,13 @@ export function middleware(req: NextRequest) {
       && !url.pathname.includes('/auth') 
       && !url.pathname.includes('/docs')) {
     
-    // if (req.method === 'OPTIONS') {
-    //   const res = NextResponse.next();
-    //   res.headers.append('Access-Control-Allow-Origin', '*');
-    //   res.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    //   res.headers.append('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
-    //   return res;
-    // }
+    if (req.method === 'OPTIONS') {
+      const res = NextResponse.next();
+      res.headers.append('Access-Control-Allow-Origin', '*');
+      res.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.headers.append('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+      return res;
+    }
     
     // Get the stored JWT in the request headers
     const jwt = req.headers.get('authorization')?.split(' ')[1];
