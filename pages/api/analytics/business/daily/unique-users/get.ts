1f8d597247f8e18745f5b18bfde494eb733f7318
TE-Khang Nguyen <khang.nguyen@talentedge.ai>
2025-03-31 19:13:52 +0000

    Sync with upstream, excluding workflows

---
+++ pages/api/analytics/business/daily/unique-users/get.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { createApiClient } from "@/libs/supabase/supabaseApi";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "GET") {
+        return res.status(405).json({ error: "Method not allowed!" });
+    }
+
+    const business_id = req.query["business-id"];
+
+    if (!business_id) {
+        return res.status(400).json({ error: "Business ID is required" });
+    }
+
+    // Extract authorization token
+    const token = req.headers.authorization?.split(" ")[1];
+    // Create Supabase client
+    const supabase = createApiClient(token);
+    
+    try {
+        const { 
+            data: queryData, 
+            error 
+        } = await supabase
+            .from("analytics_daily_unique_users")
+            .select("*")
+            .eq("business_id", business_id);
+
+        if (error) {
+            return res.status(400).json({ error: error.message });
+        }
+
+        return res.status(200).json({ data: queryData });
+    } catch (err: any) {
+        return res.status(500).json({ error: err.message || "An error has occurred while retrieving the challenge information."});
+    }
+
+};
