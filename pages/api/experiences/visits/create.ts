5fce9e131dbf52be842a975dcb6f4d835509a7fa
TE-Khang Nguyen <khang.nguyen@talentedge.ai>
2025-04-17 07:28:14 +0000

    Sync with upstream, excluding workflows

--- pages/api/experiences/visits/create.ts
+++ pages/api/experiences/visits/create.ts
@@ -2,56 +2,82 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { createApiClient } from "@/libs/supabase/supabaseApi";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    if (req.method !== 'POST') {
-        res.status(405).send({ message: 'Only POST requests allowed' })
-        return
-    }
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' })
+    return
+  }
 
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        const supabase = createApiClient(token);
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+    const supabase = createApiClient(token);
 
-        const { data: { user } } = await supabase.auth.getUser(token);
+    const { data: { user } } = await supabase.auth.getUser(token);
 
-        const { experience_id } = req.body;
- 
-        const { data, error } = await supabase
+    const { experience_id } = req.body;
+
+    const { data: visitData } = await supabase
+      .from("visits")
+      .select('*')
+      .eq("user_id", user!.id)
+      .eq("experience_id", experience_id)
+      .single();
+
+    if (!visitData) {
+      const { data, error } = await supabase
         .from("visits")
-        .upsert({
-              experience_id,
-              user_id: user!.id,
-        },
-        { onConflict: 'id' })
-        .select("created_at, experience_id, is_visited")
+        .insert({
+          experience_id,
+          user_id: user!.id,
+          is_visited: true,
+        })
+        .select("*")
         .single();
 
         if (error) {
-            return res.status(400).json({
-                success: false,
-                error: error.message
-            });
+          return res.status(400).json({
+            error: error.message
+          });
         }
-
+  
         return res.status(200).json({
-            data: data,
-            success: true
+          data: data
         });
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            error: 'Internal server error'
+    } else {
+      const { data, error } = await supabase
+        .from("visits")
+        .update({
+          is_visited: !visitData.is_visited,
+        })
+        .eq("id", visitData.id)
+        .select("*")
+        .single();
+
+      if (error) {
+        return res.status(400).json({
+          error: error.message
         });
+      }
+
+      return res.status(200).json({
+        data: data
+      });
     }
+
+  } catch (error: any) {
+    return res.status(500).json({
+      error: 'Internal server error'
+    });
+  }
 }
 
 // Workaround to enable Swagger on production 
 export const swaggerExpVisitsCreate = {
-    index:17, 
-    text:
-`"/api/v1/experiences/visits/": {
+  index: 17,
+  text:
+    `"/api/v1/experiences/visits/": {
     "post": {
       "tags": ["visits"],
       "summary": "Store user's visits to an experience.",
@@ -87,10 +113,16 @@ export const swaggerExpVisitsCreate = {
                   "data": {
                     "type": "object",
                     "properties": {
-                      "created_at": {
+                      "id": {
+                        "type": "integer"
+                      },
+                      "user_id": {
                         "type": "string"
                       },
                       "experience-id": {
+                        "type": "string"
+                      },
+                      "created_at": {
                         "type": "string"
                       },
                       "is_visited": {
