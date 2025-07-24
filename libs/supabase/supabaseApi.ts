29115efe2cfc8f2d3d1d27ff0cb7c9b7aeea03cf
TE-Khang Nguyen <khang.nguyen@talentedge.ai>
2025-07-24 09:13:43 +0000

    Sync with upstream, excluding workflows

--- libs/supabase/supabaseApi.ts
+++ libs/supabase/supabaseApi.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 //add type of variable later
-export const createApiClient = (token: string) => {
+export const createApiClient = (token: any) => {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
